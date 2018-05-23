9658161db5d6a31bbaf97381b0df4e9d5180fb0c
Futa Hirakoba <korosuke613613@gmail.com>
2018-05-24 00:12:12 +0900

    add sigmoid tanh

--- python/bpnn_symm.py
+++ python/bpnn_symm.py
@@ -13,37 +13,52 @@ import string
 
 random.seed(0)
 
+
 # calculate a random number where:  a <= rand < b
 def rand(a, b):
-    return (b-a)*random.random() + a
+    return (b - a) * random.random() + a
+
 
 # Make a matrix (we could use NumPy to speed this up)
 def makeMatrix(I, J, fill=0.0):
     m = []
     for i in range(I):
-        m.append([fill]*J)
+        m.append([fill] * J)
     return m
 
-# our sigmoid function, tanh is a little nicer than the standard 1/(1+e^-x)
+
+# sigmoid function
 def sigmoid(x):
+    return 1.0 / (1.0 + math.exp(-x))
+
+
+# our sigmoid function, tanh is a little nicer than the standard 1/(1+e^-x)
+def tanh(x):
     return math.tanh(x)
+
 
 # derivative of our sigmoid function, in terms of the output (i.e. y)
 def dsigmoid(y):
-    return 1.0 - y**2
+    return y * (1.0 - y)
+
+
+# derivative of our sigmoid function, in terms of the output (i.e. y)
+def dtanh(y):
+    return 1.0 - y ** 2
+
 
 class NN:
     def __init__(self, ni, nh, no):
         # number of input, hidden, and output nodes
-        self.ni = ni + 1 # +1 for bias node
+        self.ni = ni + 1  # +1 for bias node
         self.nh = nh
         self.no = no
 
         # activations for nodes
-        self.ai = [1.0]*self.ni
-        self.ah = [1.0]*self.nh
-        self.ao = [1.0]*self.no
-        
+        self.ai = [1.0] * self.ni
+        self.ah = [1.0] * self.nh
+        self.ao = [1.0] * self.no
+
         # create weights
         self.wi = makeMatrix(self.ni, self.nh)
         self.wo = makeMatrix(self.nh, self.no)
@@ -58,14 +73,18 @@ class NN:
         # last change in weights for momentum   
         self.ci = makeMatrix(self.ni, self.nh)
         self.co = makeMatrix(self.nh, self.no)
+        self.activation = sigmoid
+        self.dactivation = dsigmoid
 
-    def update(self, inputs):
-        if len(inputs) != self.ni-1:
+    def update(self, inputs, activation=None):
+        if len(inputs) != self.ni - 1:
             raise ValueError('wrong number of inputs')
 
+        if activation is None:
+            activation = self.activation
+
         # input activations
-        for i in range(self.ni-1):
-            #self.ai[i] = sigmoid(inputs[i])
+        for i in range(self.ni - 1):
             self.ai[i] = inputs[i]
 
         # hidden activations
@@ -73,57 +92,58 @@ class NN:
             sum = 0.0
             for i in range(self.ni):
                 sum = sum + self.ai[i] * self.wi[i][j]
-            self.ah[j] = sigmoid(sum)
+            self.ah[j] = activation(sum)
 
         # output activations
         for k in range(self.no):
             sum = 0.0
             for j in range(self.nh):
                 sum = sum + self.ah[j] * self.wo[j][k]
-            self.ao[k] = sigmoid(sum)
+            self.ao[k] = activation(sum)
 
         return self.ao[:]
 
-
-    def backPropagate(self, targets, N, M):
+    def backPropagate(self, targets, N, M, dactivation=None):
         if len(targets) != self.no:
             raise ValueError('wrong number of target values')
+
+        if dactivation is None:
+            dactivation = self.dactivation
 
         # calculate error terms for output
         output_deltas = [0.0] * self.no
         for k in range(self.no):
-            error = targets[k]-self.ao[k]
-            output_deltas[k] = dsigmoid(self.ao[k]) * error
+            error = targets[k] - self.ao[k]
+            output_deltas[k] = dactivation(self.ao[k]) * error
 
         # calculate error terms for hidden
         hidden_deltas = [0.0] * self.nh
         for j in range(self.nh):
             error = 0.0
             for k in range(self.no):
-                error = error + output_deltas[k]*self.wo[j][k]
-            hidden_deltas[j] = dsigmoid(self.ah[j]) * error
+                error = error + output_deltas[k] * self.wo[j][k]
+            hidden_deltas[j] = dactivation(self.ah[j]) * error
 
         # update output weights
         for j in range(self.nh):
             for k in range(self.no):
-                change = output_deltas[k]*self.ah[j]
-                self.wo[j][k] = self.wo[j][k] + N*change + M*self.co[j][k]
+                change = output_deltas[k] * self.ah[j]
+                self.wo[j][k] = self.wo[j][k] + N * change + M * self.co[j][k]
                 self.co[j][k] = change
-                #print N*change, M*self.co[j][k]
+                # print N*change, M*self.co[j][k]
 
         # update input weights
         for i in range(self.ni):
             for j in range(self.nh):
-                change = hidden_deltas[j]*self.ai[i]
-                self.wi[i][j] = self.wi[i][j] + N*change + M*self.ci[i][j]
+                change = hidden_deltas[j] * self.ai[i]
+                self.wi[i][j] = self.wi[i][j] + N * change + M * self.ci[i][j]
                 self.ci[i][j] = change
 
         # calculate error
         error = 0.0
         for k in range(len(targets)):
-            error = error + 0.5*(targets[k]-self.ao[k])**2
+            error = error + 0.5 * (targets[k] - self.ao[k]) ** 2
         return error
-
 
     def test(self, patterns):
         for p in patterns:
@@ -155,93 +175,96 @@ class NN:
 def demo():
     # Teach network XOR function
     pat = [
-        [[0,0], [0]],
-        [[0,1], [1]],
-        [[1,0], [1]],
-        [[1,1], [0]]
+        [[0, 0], [0]],
+        [[0, 1], [1]],
+        [[1, 0], [1]],
+        [[1, 1], [0]]
     ]
 
     # create a network with two input, two hidden, and one output nodes
     n = NN(2, 2, 1)
+    n.activation = tanh
+    n.dactivation = dtanh
     # train it with some patterns
-    n.train(pat)
+    n.train(pat, iterations=10000)
     # test it
     n.test(pat)
-
 
 
 def demo_symmetry_detection():
     # Teach network XOR function
 
     pat = [
-        [[0,0,0,0,0,0],[1]],
-        [[0,0,0,0,0,1],[0]],
-        [[0,0,0,0,1,0],[0]],
-        [[0,0,0,0,1,1],[0]],
-        [[0,0,0,1,0,0],[0]],
-        [[0,0,0,1,0,1],[0]],
-        [[0,0,0,1,1,0],[0]],
-        [[0,0,0,1,1,1],[0]],
-        [[0,0,1,0,0,0],[0]],
-        [[0,0,1,0,0,1],[0]],
-        [[0,0,1,0,1,0],[0]],
-        [[0,0,1,0,1,1],[0]],
-        [[0,0,1,1,0,0],[1]],
-        [[0,0,1,1,0,1],[0]],
-        [[0,0,1,1,1,0],[0]],
-        [[0,0,1,1,1,1],[0]],
-        [[0,1,0,0,0,0],[0]],
-        [[0,1,0,0,0,1],[0]],
-        [[0,1,0,0,1,0],[1]],
-        [[0,1,0,0,1,1],[0]],
-        [[0,1,0,1,0,0],[0]],
-        [[0,1,0,1,0,1],[0]],
-        [[0,1,0,1,1,0],[0]],
-        [[0,1,0,1,1,1],[0]],
-        [[0,1,1,0,0,0],[0]],
-        [[0,1,1,0,0,1],[0]],
-        [[0,1,1,0,1,0],[0]],
-        [[0,1,1,0,1,1],[0]],
-        [[0,1,1,1,0,0],[0]],
-        [[0,1,1,1,0,1],[0]],
-        [[0,1,1,1,1,0],[1]],
-        [[0,1,1,1,1,1],[0]],
-        [[1,0,0,0,0,0],[0]],
-        [[1,0,0,0,0,1],[1]],
-        [[1,0,0,0,1,0],[0]],
-        [[1,0,0,0,1,1],[0]],
-        [[1,0,0,1,0,0],[0]],
-        [[1,0,0,1,0,1],[0]],
-        [[1,0,0,1,1,0],[0]],
-        [[1,0,0,1,1,1],[0]],
-        [[1,0,1,0,0,0],[0]],
-        [[1,0,1,0,0,1],[0]],
-        [[1,0,1,0,1,0],[0]],
-        [[1,0,1,0,1,1],[0]],
-        [[1,0,1,1,0,0],[0]],
-        [[1,0,1,1,0,1],[1]],
-        [[1,0,1,1,1,0],[0]],
-        [[1,0,1,1,1,1],[0]],
-        [[1,1,0,0,0,0],[0]],
-        [[1,1,0,0,0,1],[0]],
-        [[1,1,0,0,1,0],[0]],
-        [[1,1,0,0,1,1],[1]],
-        [[1,1,0,1,0,0],[0]],
-        [[1,1,0,1,0,1],[0]],
-        [[1,1,0,1,1,0],[0]],
-        [[1,1,0,1,1,1],[0]],
-        [[1,1,1,0,0,0],[0]],
-        [[1,1,1,0,0,1],[0]],
-        [[1,1,1,0,1,0],[0]],
-        [[1,1,1,0,1,1],[0]],
-        [[1,1,1,1,0,0],[0]],
-        [[1,1,1,1,0,1],[0]],
-        [[1,1,1,1,1,0],[0]],
-        [[1,1,1,1,1,1],[1]]
+        [[0, 0, 0, 0, 0, 0], [1]],
+        [[0, 0, 0, 0, 0, 1], [0]],
+        [[0, 0, 0, 0, 1, 0], [0]],
+        [[0, 0, 0, 0, 1, 1], [0]],
+        [[0, 0, 0, 1, 0, 0], [0]],
+        [[0, 0, 0, 1, 0, 1], [0]],
+        [[0, 0, 0, 1, 1, 0], [0]],
+        [[0, 0, 0, 1, 1, 1], [0]],
+        [[0, 0, 1, 0, 0, 0], [0]],
+        [[0, 0, 1, 0, 0, 1], [0]],
+        [[0, 0, 1, 0, 1, 0], [0]],
+        [[0, 0, 1, 0, 1, 1], [0]],
+        [[0, 0, 1, 1, 0, 0], [1]],
+        [[0, 0, 1, 1, 0, 1], [0]],
+        [[0, 0, 1, 1, 1, 0], [0]],
+        [[0, 0, 1, 1, 1, 1], [0]],
+        [[0, 1, 0, 0, 0, 0], [0]],
+        [[0, 1, 0, 0, 0, 1], [0]],
+        [[0, 1, 0, 0, 1, 0], [1]],
+        [[0, 1, 0, 0, 1, 1], [0]],
+        [[0, 1, 0, 1, 0, 0], [0]],
+        [[0, 1, 0, 1, 0, 1], [0]],
+        [[0, 1, 0, 1, 1, 0], [0]],
+        [[0, 1, 0, 1, 1, 1], [0]],
+        [[0, 1, 1, 0, 0, 0], [0]],
+        [[0, 1, 1, 0, 0, 1], [0]],
+        [[0, 1, 1, 0, 1, 0], [0]],
+        [[0, 1, 1, 0, 1, 1], [0]],
+        [[0, 1, 1, 1, 0, 0], [0]],
+        [[0, 1, 1, 1, 0, 1], [0]],
+        [[0, 1, 1, 1, 1, 0], [1]],
+        [[0, 1, 1, 1, 1, 1], [0]],
+        [[1, 0, 0, 0, 0, 0], [0]],
+        [[1, 0, 0, 0, 0, 1], [1]],
+        [[1, 0, 0, 0, 1, 0], [0]],
+        [[1, 0, 0, 0, 1, 1], [0]],
+        [[1, 0, 0, 1, 0, 0], [0]],
+        [[1, 0, 0, 1, 0, 1], [0]],
+        [[1, 0, 0, 1, 1, 0], [0]],
+        [[1, 0, 0, 1, 1, 1], [0]],
+        [[1, 0, 1, 0, 0, 0], [0]],
+        [[1, 0, 1, 0, 0, 1], [0]],
+        [[1, 0, 1, 0, 1, 0], [0]],
+        [[1, 0, 1, 0, 1, 1], [0]],
+        [[1, 0, 1, 1, 0, 0], [0]],
+        [[1, 0, 1, 1, 0, 1], [1]],
+        [[1, 0, 1, 1, 1, 0], [0]],
+        [[1, 0, 1, 1, 1, 1], [0]],
+        [[1, 1, 0, 0, 0, 0], [0]],
+        [[1, 1, 0, 0, 0, 1], [0]],
+        [[1, 1, 0, 0, 1, 0], [0]],
+        [[1, 1, 0, 0, 1, 1], [1]],
+        [[1, 1, 0, 1, 0, 0], [0]],
+        [[1, 1, 0, 1, 0, 1], [0]],
+        [[1, 1, 0, 1, 1, 0], [0]],
+        [[1, 1, 0, 1, 1, 1], [0]],
+        [[1, 1, 1, 0, 0, 0], [0]],
+        [[1, 1, 1, 0, 0, 1], [0]],
+        [[1, 1, 1, 0, 1, 0], [0]],
+        [[1, 1, 1, 0, 1, 1], [0]],
+        [[1, 1, 1, 1, 0, 0], [0]],
+        [[1, 1, 1, 1, 0, 1], [0]],
+        [[1, 1, 1, 1, 1, 0], [0]],
+        [[1, 1, 1, 1, 1, 1], [1]]
     ]
 
     # create a network with two input, two hidden, and one output nodes
     n = NN(6, 2, 1)
+    n.activation = tanh
+    n.dactivation = dtanh
     # train it with some patterns
     n.train(pat)
     # test it
@@ -249,4 +272,5 @@ def demo_symmetry_detection():
 
 
 if __name__ == '__main__':
-    demo_symmetry_detection()
+    # demo_symmetry_detection()
+    demo()
